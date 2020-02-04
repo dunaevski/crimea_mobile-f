3cff16b25215e510b4675eeca6c0dc39235f2ee0
Alexander Dunaevski <a.dunaevskiy@simpleone.ru>
2020-02-04 03:25:04 +0300

    Add Icons And Logos

--- App.tsx
+++ App.tsx
@@ -1,43 +1,90 @@
-import React from 'react';
-import Card from './components/Card'
-import styled from 'styled-components'
+import React from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import Logo from "./components/Logo";
+import Card from "./components/Card";
+import styled from "styled-components";
+// import {Ionicons} from '@expo/vector-icons';
+import { NotificationIcon } from "./components/Icons";
 
 export default function App() {
   return (
     <Container>
-      <TitleBar>
-        <Avatar source={require('./assets/avatar.jpg')} />
-        <Title>Добро пожаловать,</Title>
-        <Name>Username!</Name>
-      </TitleBar>
+      <SafeAreaView>
+        <ScrollView style={{ height: "100%" }}>
+          <TitleBar>
+            <Avatar source={require("./assets/avatar.jpg")} />
+            <Title>Добро пожаловать,</Title>
+            <Name>Username!</Name>
+            <NotificationIcon
+              style={{
+                position: "absolute",
+                top: 5,
+                right: 20
+              }}
+            />
+          </TitleBar>
 
-      <Subtitle>
-        Популярные места
-      </Subtitle>
-        <Card />
+          <ScrollView
+            style={{
+              flexDirection: "row",
+              padding: 20,
+              paddingLeft: 12,
+              paddingTop: 30
+            }}
+            horizontal={true}
+          >
+            <Logo
+              image={require("./assets/logo-framerx.png")}
+              text={"Framer X"}
+            />
+            <Logo image={require("./assets/logo-figma.png")} text={"Figma"} />
+          </ScrollView>
 
+          <Subtitle>Популярные места</Subtitle>
+          <ScrollView
+            horizontal={true}
+            style={{ paddingBottom: 30 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <Card
+              title={"Lorem ipsum dolor."}
+              image={require("./assets/background2.jpg")}
+              caption={"React Native"}
+              logo={require("./assets/logo-react.png")}
+              subtitle={"5 of 12 sections"}
+            />
+            <Card
+              title={"Lorem ipsum dolor."}
+              image={require("./assets/background1.jpg")}
+              caption={"React Native"}
+              logo={require("./assets/logo-react.png")}
+              subtitle={"5 of 12 sections"}
+            />
+          </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 }
 
 const Subtitle = styled.Text`
- color: #b8bece;
- font-weight: 600;
- font-size: 15px;
- margin-left: 20px;
- margin-top: 50px;
- text-transform: uppercase;
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 20px;
+  text-transform: uppercase;
 `;
 
 const Avatar = styled.Image`
- width: 44px;
- height: 44px;
- background: black;
- border-radius: 22px;
- margin-left: 20px;
- position: absolute;
- top: 0;
- left: 0;
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
 
 const Container = styled.View`
@@ -46,9 +93,9 @@ const Container = styled.View`
 `;
 
 const Title = styled.Text`
- font-size: 16px;
- color: #b8bece;
- font-weight: 500;
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
 `;
 
 const Name = styled.Text`
