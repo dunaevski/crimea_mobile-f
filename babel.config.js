387a860847ddaf46a37f7f9e5f65e17c0dac8c08
Alexander Dunaevski <a.dunaevskiy@simpleone.ru>
2020-02-09 17:35:35 +0300

    fix global imports

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    resolve: {
+      extensions: [".js", ".jsx", ".tsx", ".json"],
+      modules: [path.resolve("./src"), path.resolve("./node_modules")]
+    }
   };
 };
