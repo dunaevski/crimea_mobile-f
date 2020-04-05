345d81f8ea9d5eea3d95daef831aeb52e7a30f60
Alexander Dunaevski <a.dunaevskiy@simpleone.ru>
2020-04-05 03:59:44 +0300

    fix Avatar url

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,19 +3,18 @@ import { observable } from 'mobx';
 import { inject, observer } from 'mobx-react';
 import styled from 'styled-components';
 
-
 @inject('UIStore')
 @observer
 class Avatar extends Component {
   @observable photo = "https://cl.ly/55da82beb939/download/avatar-default.jpg";
 
   componentDidMount() {
-    // fetch("https://uinames.com/api/?ext&region=russia")
-    //   .then(response => response.json())
-    //   .then(response => {
-    //     this.photo = response.photo;
-    //     this.props.UIStore.setName(response.name);
-    //   });
+    fetch("https://randomuser.me/api/")
+      .then(response => response.json())
+      .then(response => {
+        this.photo =response.results[0].picture.medium;
+        this.props.UIStore.setName(`${response.results[0].name.first} ${response.results[0].name.last}`);
+      });
   }
 
   render() {
